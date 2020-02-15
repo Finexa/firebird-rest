e0e6dfc2dbd49cbe8959920a8b176f75b7b90dbb
Felix Schoeler <felix@finexa.no>
2020-02-15 12:56:54 +0100

    Replaced express with fastify

--- src/main.js
+++ src/main.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const compression = require('compression');
-const bodyParser = require('body-parser');
 const sqlQuery = require('./utils/sqlQuery');
 const port = require('./utils/flagParams').serverPort();
-const app = express();
+const app = require('fastify')();
 
-app.use(compression()); // attempt to compress response bodies
-app.use(bodyParser.urlencoded({ extended: false, limit: '1mb' })); // parsing the URL-encoded data with the ```querystring``` library
-app.use(bodyParser.json({ limit: '1mb' })); // maximum request body size
-app.use((req, res, next) => {
-  req.connection.setTimeout(2 * 60 * 1000); // 2 minutes
-  res.connection.setTimeout(2 * 60 * 1000); // 2 minutes
-  next();
-});
-
-app.get('/', sqlQuery('query')); // parse SQL queries via http GET request
 app.post('/', sqlQuery('body')); // parse SQL queries via http POST request
 
 // RUN FIREBIRD SERVER
