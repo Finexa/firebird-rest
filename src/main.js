4b2582d0f5ba3da76fce824586baaa310ef6be81
Felix Schoeler <felix@finexa.no>
2020-06-25 10:02:45 +0200

    Added redbird

--- src/main.js
+++ src/main.js
@@ -12,3 +12,16 @@ app.post('/', sqlQuery('body')); // parse SQL queries via http POST request
 
 // RUN FIREBIRD SERVER
 app.listen(port, '0.0.0.0', () => console.log(`Firebird Server up and running on\nhttp://localhost:${port}`));
+
+const proxy = require('redbird')({ port: 443, xfwd: false });
+
+proxy.register('firebird.finexa.no', 'http://127.0.0.1:4444', {
+  port: 80,
+  ssl: {
+    letsencrypt: {
+      email: 'felix@finexa.no',
+      production: false,
+    },
+    http2: true,
+  },
+});
