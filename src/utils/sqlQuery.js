19f74a4dd211d4090e078466cc9845e91f6addd3
Felix Schoeler <felix@finexa.no>
2020-11-18 15:18:24 +0100

    Added possibility to execute transactions

--- src/utils/sqlQuery.js
+++ src/utils/sqlQuery.js
@@ -14,49 +14,99 @@ const sqlQuery = param => {
     Options.password = properties.password || Options.password;
     Options.role = properties.role || Options.role;
 
-    const sql = properties.sql;
-    const params = parseDateStrings(properties.params);
+    const isTransaction = properties.isTransaction;
+    const statements = properties.statements;
 
-    if (!sql) {
-      return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+    if (isTransaction) {
+      if (!statements || !statements.length) {
+        return res.send(['No valid SQL statements found! Please enter valid SQL statements.']);
+      }
+    } else {
+      if (!properties.sql) {
+        return res.send(['No valid SQL query found! Please enter a valid SQL query.']);
+      }
     }
+
 
     Firebird.attach(Options, (err, db) => {
       if (err) {
         console.error(err);
-        db.detach();
+        if (db) {
+          db.detach()
+        }
         res.status(400); // BAD REQUEST
         return res.send(`\n${err.message}\n`);
       }
 
-      db.query(sql, params, (err, data) => {
-        if (err) {
-          db.detach();
-          res.status(400); // BAD REQUEST
-          return res.send(`\n${err.message}\n`);
-        }
-        db.detach();
-
-        if (data) {
-          if (Array.isArray(data)) {
-            // CONVERT RAW QUERY RESULT AND RETURN JSON
-            data.forEach(row => {
-              const newRow = convertRow(row);
-              result.push(newRow);
-            });
-          } else {
-            const newRow = convertRow(data);
-            result = newRow;
+      if (isTransaction) {
+        db.transaction(Firebird.ISOLATION_REPEATABLE_READ, async (err, transaction) => {
+          if (err) {
+            db.detach();
+            res.status(400); // BAD REQUEST
+            return res.send(`\n${err.message}\n`);
           }
-        }
 
-        const jsonString = bufferJson.stringify(result);
+          for(const statement of statements) {
+            const { params, sql } = statement;
+            await executeTransactionQuery(transaction, { params: parseDateStrings(params), sql });
+          }
 
-        return res.send(JSON.parse(jsonString));
-      });
+          transaction.commit((err) => {
+            if (err) {
+              res.status(400);
+              res.send(`\n${err.message}\n`);
+            } else {
+              res.status(200);
+              res.send('OK');
+            }
+          })
+        })
+      } else {
+        const params = parseDateStrings(properties.params);
+        const sql = properties.sql;
+        db.query(sql, params, (err, data) => {
+          if (err) {
+            db.detach();
+            res.status(400); // BAD REQUEST
+            return res.send(`\n${err.message}\n`);
+          }
+          db.detach();
+  
+          if (data) {
+            if (Array.isArray(data)) {
+              // CONVERT RAW QUERY RESULT AND RETURN JSON
+              data.forEach(row => {
+                const newRow = convertRow(row);
+                result.push(newRow);
+              });
+            } else {
+              const newRow = convertRow(data);
+              result = newRow;
+            }
+          }
+  
+          const jsonString = bufferJson.stringify(result);
+  
+          return res.send(JSON.parse(jsonString));
+        });
+      }
     });
   };
 };
+
+function executeTransactionQuery(transaction, statement) {
+  const { sql, params } = statement;
+  return new Promise((resolve, reject) => {
+    transaction.query(sql, params, (err, result) => {
+      if (err) {
+        transaction.rollback();
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    })
+  });
+} 
 
 function convertRow(row) {
   let newRow = {};
