27959b0f66fa771d0eeddf0f79a52fef67780741
Felix Hanken <felix@finexa.no>
2025-04-10 12:19:27 +0000

    Added dual server ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,28 @@
 module.exports = {
     apps: [
       {
-        name: "firebird-rest",
+        name: "firebird-rest-01",
         script: "src/main.js",
         autorestart: true,
-        max_memory_restart: "1G",
+        max_memory_restart: "2G",
         env: {
           PORT: 4243,
+          PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
         },
         time: true,
+        interpreter: "bun",
+      },
+      {
+        name: "firebird-rest-02",
+        script: "src/main.js",
+        autorestart: true,
+        max_memory_restart: "2G",
+        env: {
+          PORT: 4244,
+          PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
+        },
+        time: true,
+        interpreter: "bun",
       }
     ]
   }
--- pm2.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "firebird-reste", // Name of your application
-  script: "src/main.js", // Entry point of your application
-  interpreter: "bun", // Bun interpreter
-  env: {
-    PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-  }
-};
